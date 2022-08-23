67a01c0ea656df0c20ffca3a3875e7f732bdd392
Samuel Baraka <sababuvercetti@gmail.com>
2022-08-23 11:41:50 +0300

    Added a rest client class flutter style

---
+++ Elgato/Utilities/RestClient.swift
@@ -0,0 +1,14 @@
+//
+//  RestClient.swift
+//  Elgato
+//
+//  Created by Samuel Baraka on 23/08/2022.
+//
+
+import Foundation
+
+
+class RestClient {
+    var baseUrl:String="https://api.thecatapi.com/v1/"
+    
+}
