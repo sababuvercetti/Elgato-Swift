027c0421ff6f2bde632d5ddd3b47e5dbd6073702
Samuel Baraka <sababuvercetti@gmail.com>
2022-08-23 11:09:20 +0300

    Initial Commit

---
+++ Elgato/ElgatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElgatoApp.swift
+//  Elgato
+//
+//  Created by Samuel Baraka on 23/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ElgatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
