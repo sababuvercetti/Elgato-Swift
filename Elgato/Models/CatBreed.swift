a39db71eab7ebb386da90aaadb26a9e45f48d9e4
Samuel Baraka <sababuvercetti@gmail.com>
2022-08-23 13:23:07 +0300

    Added breeds model

---
+++ Elgato/Models/CatBreed.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Elgato
+//
+//  Created by Samuel Baraka on 23/08/2022.
+//
+
+import Foundation
+
+
+struct CatBreed : Hashable,Codable{
+    let name: String
+    let description: String
+}
