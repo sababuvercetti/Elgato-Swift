a2529dbea4c0dbb68fad29a043fb5725826b7d2e
Samuel Baraka <sababuvercetti@gmail.com>
2022-08-25 11:44:27 +0300

    Abstracted new views

---
+++ Elgato/Breeds/BreedsView.swift
@@ -0,0 +1,20 @@
+//
+//  BreedsView.swift
+//  Elgato
+//
+//  Created by Samuel Baraka on 23/08/2022.
+//
+
+import SwiftUI
+
+struct BreedsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BreedsView_Previews: PreviewProvider {
+    static var previews: some View {
+        BreedsView()
+    }
+}
---
+++ Elgato/Breeds/BreedsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  BreedsViewModel.swift
+//  Elgato
+//
+//  Created by Samuel Baraka on 23/08/2022.
+//
+
+import Foundation
