ef265c13c724b47548cb0705449739aa47b813ea
Samuel Baraka <sababuvercetti@gmail.com>
2022-08-25 11:45:32 +0300

    Added cat image file

--- Elgato/Models/CatBreed.swift
+++ Elgato/Models/CatBreed.swift
@@ -11,4 +11,10 @@ import Foundation
 struct CatBreed : Hashable,Codable{
     let name: String
     let description: String
+    let image: CatImage?
+}
+
+
+struct CatImage : Hashable, Codable{
+    let url: String
 }
